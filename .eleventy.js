3cdf514d7e5c4a08a57aa1f3e1260e077acbb29a
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-07-14 15:54:21 -0700

    Utilizing njk templating lang, beginning layout implementation

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = ( config ) => {
   // Pass throughs
   config.addPassthroughCopy('./src/assets/css/'); 
   return { 
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
     dir: {
       input: "src",
       output: "dist"
